45cc2a19d1b71f62b03d1b30c40fe10ba028101d
andersonmarin <anderson.marin@outlook.com>
2025-08-22 23:15:29 -0300

    Enhance Home page search functionality: add state management, dynamic search type, and API integration.

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,51 @@
+"use client";
+
+import {useState} from "react";
+
 export default function Home() {
+    const availableSearchTypes = ["people", "movie"];
+
+    const [results, setResults] = useState([]);
+    const [searchType, setSearchType] = useState(availableSearchTypes[0]);
+    const [searchText, setSearchText] = useState("");
+
+    async function fetchData(searchType: typeof availableSearchTypes[number], searchText: string) {
+        const response = await fetch(`https://www.swapi.tech/api/${searchType}/?name=${searchText}`);
+        const data = await response.json();
+
+        if (data.message !== "ok") {
+            throw new Error(data.message);
+        }
+
+        return data.result;
+    }
+
+    async function search() {
+        const results = await fetchData(searchType, searchText);
+        setResults(results);
+    }
+
     return (
         <div className="flex justify-center items-start h-screen bg-gray-100 p-8 gap-8">
             <div className="flex flex-col gap-5 bg-white p-4 rounded-lg border border-gray-300 min-w-xs shadow-md">
                 <div>What are you searching for?</div>
                 <div className="flex gap-4">
-                    <label className="flex gap-1 items-center">
-                        <input type="radio" name="searchType" value="people"/>
-                        <span className="font-semibold">People</span>
-                    </label>
-                    <label className="flex gap-1 items-center">
-                        <input type="radio" name="searchType" value="movie"/>
-                        <span className="font-semibold">Movie</span>
-                    </label>
+                    {availableSearchTypes.map((type) => (
+                        <label key={type} className="flex gap-1 items-center">
+                            <input type="radio" name="searchType" value={type}
+                                   checked={searchType === type} onChange={() => setSearchType(type)}/>
+                            <span className="font-semibold capitalize">{type}</span>
+                        </label>
+                    ))}
                 </div>
                 <input className="h-8 px-2 border border-gray-300 rounded" type="text"
-                       placeholder="e.g. Chewbacca, Yoda, Boba Fett"/>
-                <button className="bg-green-600 text-white font-bold uppercase h-8 rounded-full">Search</button>
+                       placeholder="e.g. Chewbacca, Yoda, Boba Fett"
+                       value={searchText} onChange={(e) => setSearchText(e.target.value)}/>
+                <button
+                    className="bg-green-600 text-white font-bold uppercase h-8 rounded-full cursor-pointer disabled:bg-gray-300"
+                    onClick={search} disabled={searchText.length === 0}>
+                    Search
+                </button>
             </div>
             <div
                 className="flex flex-col gap-5 bg-white p-4 rounded-lg border border-gray-300 min-w-lg shadow-md">
