528ef181f8ff3c8dda5512c3c1de5e5a1625a112
andersonmarin <anderson.marin@outlook.com>
2025-08-22 22:44:46 -0300

    Add a reusable `Navbar` component and include it in the layout. Clean up global styles.

---
+++ app/_components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div className="flex justify-center items-center h-12 bg-white border-b border-gray-300">
+            <div className="text-green-600 font-bold">SWStarter</div>
+        </div>
+    );
+}
+
+export default Navbar;
